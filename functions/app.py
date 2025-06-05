4615b71d21d149726b9b0219119650389b139efa
Aqeel0317 <aqeel03319384502@gmail.com>
2025-06-05 10:54:04 +0500

    Update app.py

--- functions/app.py
+++ functions/app.py
@@ -4,9 +4,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello from Flask on Netlify!"
 
 def handler(event, context):
     return awsgi.response(app, event, context)
